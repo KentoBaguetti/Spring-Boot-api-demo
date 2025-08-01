e3039ca7adf1ce49f77dcc1a1559b8550749bc6b
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-07-31 22:03:24 -0700

    Add type annotations

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,13 +1,16 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao  {
 
+    // this arraylist acts as a mock/temp database until a real database is implemented
     private static List<Person> DB = new ArrayList<>();
 
     @Override
@@ -15,7 +18,5 @@ public class FakePersonDataAccessService implements PersonDao  {
         DB.add(new Person(id, person.getName())) ;
         return 1;
     }
-
-
 
 }
